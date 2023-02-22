d820d077588369c8138e3261e836b926f7eb05ec
Sander-317 <sander.van.meerveld@gmail.com>
2023-02-22 21:47:52 +0100

    made the student list work

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,16 @@ import LineChart from "./components/LineChart";
 import StudentList from "./components/StudentList";
 
 function App({ state, makeListOfAverage, studentList }) {
-  const nameList = studentList();
-  console.log(nameList);
   return (
     <div className="App">
       <h1>student dashboard {state.test}</h1>
-      {nameList.map((student) => (
-        <div>{student.name}</div>
-      ))}
+
       <BarChart state={state} makeListOfAverage={makeListOfAverage} />
       <LineChart state={state} makeListOfAverage={makeListOfAverage} />
-      <StudentList />
+      <ul className="name-list">
+        <h2>student list:</h2>
+        <StudentList state={state} studentList={studentList} />
+      </ul>
     </div>
   );
 }
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function StudentList() {
-  return <div>StudentList</div>;
+export default function StudentList({ studentList }) {
+  const nameList = studentList();
+  return nameList.map((student, index) => (
+    <li
+      className={"name-list-name"}
+      key={index}
+      onClick={() => console.log(student.name)}
+    >
+      {student.name}
+    </li>
+  ));
 }
+//   console.log(nameList);
+//   {}
